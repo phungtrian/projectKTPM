b7c642f22469b1c11e454832a8f3c0db96c4732e
kennguen2503 <52605206+kennguen2503@users.noreply.github.com>
2021-04-23 14:01:59 +0700

    test package

---
+++ FlyIntoTheSky/src/test/java/TestAll/TestSearch.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestAll;
+
+import com.mycompany.services.JdbcUtils;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+/**
+ *
+ * @author kenng
+ */
+public class TestSearch {
+    private static Connection conn;
+    
+    @BeforeAll
+    public static void setUpClass() {
+        try {
+            conn = JdbcUtils.getConn();
+        } catch (SQLException ex) {
+            Logger.getLogger(TestSearch.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+        if (conn != null)
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(TestSearch.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
+}
