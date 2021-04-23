5bb3949505b000279b03d3e6e2e69628a1e8d6e4
LinhLee2234 <linhlee2234@gmail.com>
2021-04-23 14:43:56 +0700

    fxml

--- FlyIntoTheSky/src/main/java/com/mycompany/flyintothesky/App.java
+++ FlyIntoTheSky/src/main/java/com/mycompany/flyintothesky/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("searchflight"), 640, 480);
+        scene = new Scene(loadFXML("home"), 600, 400);
         stage.setScene(scene);
         stage.show();
     }
