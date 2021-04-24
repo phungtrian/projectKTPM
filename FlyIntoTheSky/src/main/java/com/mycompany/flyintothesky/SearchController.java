55014b52e3097663d95c793c9b93aaa126ce65c0
phungtrian <phungtrian2000@gmail.com>
2021-04-24 23:31:29 +0700

    hàm show đẩy lên table view

--- FlyIntoTheSky/src/main/java/com/mycompany/flyintothesky/SearchController.java
+++ FlyIntoTheSky/src/main/java/com/mycompany/flyintothesky/SearchController.java
@@ -5,20 +5,28 @@ import com.mycompany.pojo.Airport;
 import com.mycompany.pojo.Flight;
 import com.mycompany.services.AirportService;
 import com.mycompany.services.JdbcUtils;
+import com.mycompany.user.SearchFlight;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 
 public class SearchController implements Initializable{
@@ -55,6 +63,96 @@ public class SearchController implements Initializable{
         } catch (SQLException ex) {
             Logger.getLogger(SearchController.class.getName()).log(Level.SEVERE, null, ex);
         }
+        loadTable();
+
+        
+        this.tbFlights.setRowFactory(obj -> {
+            TableRow row = new TableRow();
+            
+            row.setOnMouseClicked(evt -> {
+                try {
+                    Flight p = this.tbFlights.getSelectionModel().getSelectedItem();
+                    txtName.setText(p.getName());
+                    txtPrice.setText(p.getPrice().toString());
+                    
+                    
+                    Connection conn = JdbcUtils.getConn();
+                    AirportService s = new AirportService(conn);
+                    Airport c = s.getAirportById(p.getId());
+                    
+                    this.cbNoidi.getSelectionModel().select(c);
+                    
+                    conn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(SearchController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            });
+            
+            return row;
+        });
+    }
+    private void loadData(String ori, String des, Date d) {
+        try {
+            Connection conn = JdbcUtils.getConn();
+            SearchFlight s = new SearchFlight(conn);
+
+            tbFlights.setItems(FXCollections.observableList(s.FindFlightLocation(ori, des, d)));
+
+            conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(SearchController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void loadTable() {
+        TableColumn colFrom = new TableColumn("Nơi đi");
+        colFrom.setCellValueFactory(new PropertyValueFactory("origin"));
+
+        TableColumn colTo = new TableColumn("Nơi đến");
+        colTo.setCellValueFactory(new PropertyValueFactory("destination"));
+
+        TableColumn colDay = new TableColumn("Ngày bay");
+        colDay.setCellValueFactory(new PropertyValueFactory("day"));
+
+        TableColumn colTime = new TableColumn("Thời gian bay");
+        colTime.setCellValueFactory(new PropertyValueFactory("time"));
+
+        TableColumn colAction = new TableColumn();
+        colAction.setCellFactory(obj -> {
+            Button btn = new Button("Chọn");
+
+            btn.setOnAction((evt) -> {
+                Utils.getBox("Ban chac chan xoa khong?", Alert.AlertType.CONFIRMATION)
+                        .showAndWait().ifPresent(bt -> {
+                            if (bt == ButtonType.OK) {
+                                try {
+                                    TableCell cell = (TableCell) ((Button) evt.getSource()).getParent();
+                                    Product p = (Product) cell.getTableRow().getItem();
+
+                                    Connection conn = JdbcUitls.getConn();
+                                    ProductService s = new ProductService(conn);
+                                    if (s.deleteProduct(p.getId()) == true) {
+                                        Utils.getBox("SUCCESSFUL", Alert.AlertType.INFORMATION).show();
+                                        loadData("");
+                                    } else {
+                                        Utils.getBox("FAILED", Alert.AlertType.ERROR).show();
+                                    }
+                                } catch (SQLException ex) {
+                                    Logger.getLogger(SearchController.class.getName()).log(Level.SEVERE, null, ex);
+                                }
+                            }
+                        });
+
+            });
+
+            TableCell cell = new TableCell();
+            cell.setGraphic(btn);
+            return cell;
+        });
+
+        this.tbProducts.getColumns().addAll(colId, colName, colDes, colPrice, colAction);
+    }
+}
 //    static String a;
 //    public String search(){
 //        a = txtNoidi.getText();
@@ -94,6 +192,4 @@ public class SearchController implements Initializable{
 //            colDay.setCellValueFactory(new PropertyValueFactory("day"));
 //
 //            this.tbFlights.getColumns().addAll(colFrom, colTo, colTime, colDay);
-//        }
-    }
-}
+//        
